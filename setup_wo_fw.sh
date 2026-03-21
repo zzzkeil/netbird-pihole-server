f5ac796d72fd9e1d7570c6b7f5c2ed1af1132afc
keil <38099918+zzzkeil@users.noreply.github.com>
2026-03-22 00:28:39 +0100

    Make Docker installation non-interactive
    Added '-y' option to apt install commands for non-interactive installation.

--- setup_wo_fw.sh
+++ setup_wo_fw.sh
@@ -207,7 +207,7 @@ Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 fi
 
 if [[ "$systemos" = 'ubuntu' ]]; then
@@ -225,7 +225,7 @@ Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 fi
 
 systemctl start docker
